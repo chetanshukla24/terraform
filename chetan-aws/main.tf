613fb810801fb3eebcd228dadc8321c86345c728
chetanshukla24 <chetan.shukla@outlook.com>
2019-03-27 20:56:59 +1100

    updated

--- chetan-aws/main.tf
+++ chetan-aws/main.tf
@@ -3,11 +3,12 @@ provider "aws" {
   region = "ap-southeast-2"
  }
 
+
 resource "aws_instance" "server" {
   ami           = "ami-04481c741a0311bbb"
   instance_type = "t2.micro"
-
+  subnet_id = "subnet-05507ef8ee9463317"
   tags {
-    Name = "terraform-example"
+    Name = "terraform"
   }
 }
